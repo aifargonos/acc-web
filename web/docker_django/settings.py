84599227c7f91c9d9ec96ca641a6ca84993082bc
aifargonos <aifargonos@gmail.com>
2018-02-20 20:16:54 +0100

    Clean up static file management

--- web/docker_django/settings.py
+++ web/docker_django/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'docker_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(SITE_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static'),
+)
 # print(STATICFILES_DIRS)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
